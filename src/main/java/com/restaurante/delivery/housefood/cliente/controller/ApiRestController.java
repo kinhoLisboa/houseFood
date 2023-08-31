8e110e65088fd99f45c9bef4904016c21b04abca
Danifleikson Buriti Lisboa <danifleikson@hotmail.com>
2023-08-30 22:01:01 -0300

    Implementando a camada Controller.

--- src/main/java/com/restaurante/delivery/housefood/cliente/controller/ApiRestController.java
+++ src/main/java/com/restaurante/delivery/housefood/cliente/controller/ApiRestController.java
@@ -1,10 +1,18 @@
 package com.restaurante.delivery.housefood.cliente.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@RequiredArgsConstructor
+@Log4j2
 public class ApiRestController implements ApiController{
+
+    private final ClienteService clienteService;
     @Override
     public ClienteResponse criaCliente(ClienteRequest clienteRequest) {
+        log.info("[Inicial] ApiRestController-criaCliente");
         clienteService.cadastra(clienteRequest);
-
-        return new ClienteResponse();
+        log.info("[Finaliza] ApiRestController-criaCliente");
+        return new ClienteResponse(clienteRequest);
     }
 }
